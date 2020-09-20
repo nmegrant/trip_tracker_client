1591571a7b461fc5d40da8d3bf787099c46fcb34
nmegrant <nme.grant@gmail.com>
2020-09-20 11:02:34 +0200

    Dispatch call to server

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
+import { useDispatch } from "react-redux";
+import { fetchToVisitThunkCreator } from "./store/toVisit/actions";
+import { fetchVisitedThunkCreator } from "./store/visited/actions";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchVisitedThunkCreator());
+    dispatch(fetchToVisitThunkCreator());
+  }, []);
+
   return (
     <div className="App">
       <h1>Trip Tracker</h1>
