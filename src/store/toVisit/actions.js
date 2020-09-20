e503456208cbf7e710f05dba364d3c5ceeb4e6aa
nmegrant <nme.grant@gmail.com>
2020-09-20 11:15:48 +0200

    Dispatch toVisit and visited lists to reducer

--- src/store/toVisit/actions.js
+++ src/store/toVisit/actions.js
@@ -1,9 +1,17 @@
 import axios from "axios";
 
+export function toVisitFetched(toVisitCities) {
+  return {
+    type: "TO_VISITED_FETCHED",
+    payload: toVisitCities,
+  };
+}
+
 export function fetchToVisitThunkCreator() {
   return async function toVisitThunk(dispatch, getState) {
     try {
       const response = await axios.get(`http://localhost:4000/tovisit`);
+      dispatch(toVisitFetched(response.data));
       console.log(response.data);
     } catch (error) {
       console.log(`Error fetching visited: ${error}`);
--- src/store/visited/actions.js
+++ src/store/visited/actions.js
@@ -1,9 +1,17 @@
 import axios from "axios";
 
+export function visitedFetched(visitedCities) {
+  return {
+    type: "VISITED_FETCHED",
+    payload: visitedCities,
+  };
+}
+
 export function fetchVisitedThunkCreator() {
   return async function visitedThunk(dispatch, getState) {
     try {
       const response = await axios.get(`http://localhost:4000/visited`);
+      dispatch(visitedFetched(response.data));
       console.log(response.data);
     } catch (error) {
       console.log(`Error fetching visited: ${error}`);
