5768cb34f85768f71ef64a7cd2a6eaf574b07a19
nmegrant <nme.grant@gmail.com>
2020-09-20 10:56:33 +0200

    Set up thunk creators for visited and tovisit fetching

---
+++ src/store/toVisit/actions.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export function fetchToVisitThunkCreator() {
+  return async function toVisitThunk(dispatch, getState) {
+    try {
+      const response = axios.get(`http://localhost:4000/tovisit`);
+      console.log(response.data);
+    } catch (error) {
+      console.log(`Error fetching visited: ${error}`);
+    }
+  };
+}
---
+++ src/store/visited/actions.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export function fetchVisitedThunkCreator() {
+  return async function visitedThunk(dispatch, getState) {
+    try {
+      const response = axios.get(`http://localhost:4000/visited`);
+      console.log(response.data);
+    } catch (error) {
+      console.log(`Error fetching visited: ${error}`);
+    }
+  };
+}
