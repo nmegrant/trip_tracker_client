d364747c102b751a57ae42485d0d960f7ab9a446
nmegrant <nme.grant@gmail.com>
2020-09-22 00:02:09 +0200

    Set up appState store

---
+++ src/store/appState/reducer.js
@@ -0,0 +1,10 @@
+const initalState = {
+  darkMode: false,
+};
+
+export default function appStateReducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import visited from "./visited/reducer";
 import toVisit from "./toVisit/reducer";
+import appState from "./appState/reducer";
 
 export default combineReducers({
   visited,
   toVisit,
+  appState,
 });
