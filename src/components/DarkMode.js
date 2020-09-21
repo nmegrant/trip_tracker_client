95d0c700b81ad836e1947ad36d1e62d066e6f606
nmegrant <nme.grant@gmail.com>
2020-09-22 00:59:47 +0200

    Button for dark mode changes

--- src/components/DarkMode.js
+++ src/components/DarkMode.js
@@ -7,10 +7,12 @@ export default function DarkMode() {
   const dispatch = useDispatch();
   const currentMode = useSelector(selectAppState()).darkMode;
 
+  const mode = currentMode ? "Light" : "Dark";
+
   function changeMode(event) {
     event.preventDefault();
     dispatch(changeDarkMode(!currentMode));
   }
 
-  return <button onClick={changeMode}>Dark Mode</button>;
+  return <button onClick={changeMode}>{mode} Mode</button>;
 }
