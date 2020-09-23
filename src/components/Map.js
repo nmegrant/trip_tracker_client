e46d8b91acaa5659fdd712925d6166356cf06667
nmegrant <nme.grant@gmail.com>
2020-09-23 20:23:25 +0200

    Basic map display

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,19 +1,18 @@
 import React, { useState } from "react";
 import GoogleMapReact from "google-map-react";
-
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+import Marker from "./Marker/Marker";
 
 export default function Map(props) {
   const [center, setCenter] = useState({ lat: 41, lng: -36 });
   const [zoom, setZoom] = useState(1);
   return (
-    <div style={{ height: "200vh", width: "100%" }}>
+    <div style={{ height: "200vh", width: "100%", margin: "10px" }}>
       <GoogleMapReact
         bootstrapURLKeys={{ key: process.env.REACT_APP_API_KEY }}
         defaultCenter={center}
         defaultZoom={zoom}
       >
-        <AnyReactComponent lat={11.0168} lng={76.9558} text="My Marker" />
+        <Marker lat={37.5665} lng={126.978} name="My Marker" color="blue" />
       </GoogleMapReact>
     </div>
   );
