8adea4e4f04dfeb1e80e4c1b58f483a4ba1540ae
nmegrant <nme.grant@gmail.com>
2020-09-23 20:02:34 +0200

    Updating starting settings

--- src/components/Map.js
+++ src/components/Map.js
@@ -4,10 +4,10 @@ import GoogleMapReact from "google-map-react";
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 export default function Map(props) {
-  const [center, setCenter] = useState({ lat: 11.0168, lng: 76.9558 });
-  const [zoom, setZoom] = useState(11);
+  const [center, setCenter] = useState({ lat: 41, lng: -36 });
+  const [zoom, setZoom] = useState(1);
   return (
-    <div style={{ height: "100vh", width: "100%" }}>
+    <div style={{ height: "200vh", width: "100%" }}>
       <GoogleMapReact
         bootstrapURLKeys={{ key: process.env.REACT_APP_API_KEY }}
         defaultCenter={center}
