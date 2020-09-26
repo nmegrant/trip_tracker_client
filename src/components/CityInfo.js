7d4611ad57c51ea4e34a13a99bb907f959af9851
nmegrant <nme.grant@gmail.com>
2020-09-26 13:16:23 +0200

    Add city info

--- src/components/CityInfo.js
+++ src/components/CityInfo.js
@@ -1,21 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { selectVisted } from "../store/visited/selectors";
 
 export default function CityInfo() {
+  const [facts, setFacts] = useState([]);
+
   const visited = useSelector(selectVisted());
   const cities = visited.map((place) => place.city);
 
-  //largest city in the world by area - Tokyo, Japan
-  //largest city in the world by population - Shanghai, China
-  //hottest city in the world - Mecca, Saudi Arabia
-  //coldest city in the world - Yakutsk, Russia
-  //windiest city in the world - Wellington, New Zealand
-  //wettest city in the world - Mawsynram, India
-  //driest city in the world - Arica, Chile
-  //sunniest city in the world - Yuma, USA
-  //highest city in the world - La Paz, Bolivia
-  //lowest city in the world - Baku, Azerbaijan
-  //most visited city in the world - Bangkok, Thailand
-  return <h1>City Info</h1>;
+  if (cities.includes("Tokyo")) {
+    setFacts([
+      ...facts,
+      "You've been to Tokyo, Japan, the largest city in the world by area.",
+    ]);
+  }
+  if (cities.includes("Shanghai")) {
+    setFacts([
+      ...facts,
+      "You've been to Shanghai, China, the largest city in the world by population.",
+    ]);
+  }
+  if (cities.includes("Mecca")) {
+    setFacts([
+      ...facts,
+      "You've been to Mecca, Saudi Arabia, the hottest city in the world.",
+    ]);
+  }
+  if (cities.includes("Yakutsk")) {
+    setFacts([
+      ...facts,
+      "You've been to Yakutsk, Russia, the coldest city in the world.",
+    ]);
+  }
+  if (cities.includes("Wellington")) {
+    setFacts([
+      ...facts,
+      "You've been to Wellington, New Zealand, the windiest city in the world.",
+    ]);
+  }
+  if (cities.includes("Mawsynram")) {
+    setFacts([
+      ...facts,
+      "You've been to Mawsynram, India, the wettest city in the world.",
+    ]);
+  }
+  if (cities.includes("Arica")) {
+    setFacts([
+      ...facts,
+      "You've been to Arica, Chile, the driest city in the world.",
+    ]);
+  }
+  if (cities.includes("Yuma")) {
+    setFacts([
+      ...facts,
+      "You've been to Yuma, USA, the sunniest city in the world.",
+    ]);
+  }
+  if (cities.includes("La Paz")) {
+    setFacts([
+      ...facts,
+      "You've been to La Paz, Bolivia, the highest city in the world.",
+    ]);
+  }
+  if (cities.includes("Baku")) {
+    setFacts([
+      ...facts,
+      "You've been to Baku, Azerbaijan, the lowest city in the world.",
+    ]);
+  }
+  if (cities.includes("Bangkok")) {
+    setFacts([
+      ...facts,
+      "You've been to Bangkok, Thailand, the most visited city in the world.",
+    ]);
+  }
+
+  return (
+    <div>
+      <h1>City Info</h1>
+    </div>
+  );
 }
