1e5b566f4187ef4d5bb3e5e164c4d36538b5abcc
nmegrant <nme.grant@gmail.com>
2020-09-26 17:33:14 +0200

    Display info on cities visited

--- src/components/CityInfo.js
+++ src/components/CityInfo.js
@@ -4,80 +4,79 @@ import { selectVisted } from "../store/visited/selectors";
 
 export default function CityInfo() {
   const [facts, setFacts] = useState([]);
-
   const visited = useSelector(selectVisted());
-  const cities = visited.map((place) => place.city);
 
   useEffect(() => {
+    const cities = visited.map((place) => place.city);
+
     if (cities.includes("Tokyo")) {
-      setFacts([
+      setFacts((facts) => [
         ...facts,
         "You've been to Tokyo, Japan, the largest city in the world by area.",
       ]);
     }
     if (cities.includes("Shanghai")) {
-      setFacts([
+      setFacts((facts) => [
         ...facts,
         "You've been to Shanghai, China, the largest city in the world by population.",
       ]);
     }
     if (cities.includes("Mecca")) {
-      setFacts([
+      setFacts((facts) => [
         ...facts,
         "You've been to Mecca, Saudi Arabia, the hottest city in the world.",
       ]);
     }
     if (cities.includes("Yakutsk")) {
-      setFacts([
+      setFacts((facts) => [
         ...facts,
         "You've been to Yakutsk, Russia, the coldest city in the world.",
       ]);
     }
     if (cities.includes("Wellington")) {
-      setFacts([
+      setFacts((facts) => [
         ...facts,
         "You've been to Wellington, New Zealand, the windiest city in the world.",
       ]);
     }
     if (cities.includes("Mawsynram")) {
-      setFacts([
+      setFacts((facts) => [
         ...facts,
         "You've been to Mawsynram, India, the wettest city in the world.",
       ]);
     }
+
     if (cities.includes("Arica")) {
-      setFacts([
+      setFacts((facts) => [
         ...facts,
         "You've been to Arica, Chile, the driest city in the world.",
       ]);
     }
     if (cities.includes("Yuma")) {
-      setFacts([
+      setFacts((facts) => [
         ...facts,
         "You've been to Yuma, USA, the sunniest city in the world.",
       ]);
     }
     if (cities.includes("La Paz")) {
-      setFacts([
+      setFacts((facts) => [
         ...facts,
         "You've been to La Paz, Bolivia, the highest city in the world.",
       ]);
     }
     if (cities.includes("Baku")) {
-      setFacts([
+      setFacts((facts) => [
         ...facts,
         "You've been to Baku, Azerbaijan, the lowest city in the world.",
       ]);
     }
     if (cities.includes("Bangkok")) {
-      setFacts([
+      setFacts((facts) => [
         ...facts,
         "You've been to Bangkok, Thailand, the most visited city in the world.",
       ]);
     }
-  }, []);
-
-  console.log(facts);
+  }, [visited]);
 
   return (
     <div>
@@ -85,7 +84,7 @@ export default function CityInfo() {
       {facts.length === 0 ? (
         <p>No city info yet!</p>
       ) : (
-        facts.map((fact) => <p>fact</p>)
+        facts.map((fact, index) => <p key={index}>{fact}</p>)
       )}
     </div>
   );
