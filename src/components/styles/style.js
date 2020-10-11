765dca9c2c0fa74941577f8e725ff3869efc928f
nmegrant <nme.grant@gmail.com>
2020-10-11 13:59:42 +0200

    Style mode button with styled components

--- src/components/styles/style.js
+++ src/components/styles/style.js
@@ -1,7 +1,33 @@
 import styled from "styled-components";
 
 export const SearchButton = styled.button`
-  background-color: black;
-  font-size: 32px;
-  color: white;
+  background-color: #00916e;
+  color: #feefe5;
+  font-size: 16px;
+  border: none;
+  border-radius: 8px;
+  padding: 5px 10px;
+  border: solid 2px #00916e;
+  transition-duration: 0.4s;
+  &:hover {
+    background-color: #feefe5;
+    color: #00916e;
+    border: solid 2px #00916e;
+  }
+`;
+
+export const ModeButton = styled.button`
+  background-color: #2274a5;
+  color: #feefe5;
+  font-size: 16px;
+  border: none;
+  border-radius: 8px;
+  padding: 5px 10px;
+  margin: 5px;
+  border: solid 2px #2274a5;
+  transition-duration: 0.4s;
+  &:active {
+    box-shadow: 0 5px #666;
+    transform: translateY(4px);
+  }
 `;
