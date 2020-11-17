042686e1258cf119444356fbbd53b92d3e02f6f1
nmegrant <nme.grant@gmail.com>
2020-11-17 22:29:40 +0100

    Create user page

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,5 +1,12 @@
 import React from "react";
+import { selectUser } from "../store/user/selectors";
+import { useSelector } from "react-redux";
 
 export default function UserPage() {
-  return <div>User Page!</div>;
+  const user = useSelector(selectUser());
+  return (
+    <div>
+      <h1>{`Welcome ${user.firstName}, to your trip planning and tracking page`}</h1>
+    </div>
+  );
 }
