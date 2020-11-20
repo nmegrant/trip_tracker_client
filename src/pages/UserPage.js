3f9a5ff6c8f37bc243eafcbe5a32874ff7ab60f4
nmegrant <nme.grant@gmail.com>
2020-11-20 17:36:57 +0100

    Redirect from user page when token invalid

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import { selectUser } from "../store/user/selectors";
 import { useSelector } from "react-redux";
 
 export default function UserPage() {
   const user = useSelector(selectUser());
+  const history = useHistory();
+
+  useEffect(() => {
+    if (user.token === null) {
+      history.push("/login");
+    }
+  }, [user]);
   return (
     <div>
       <h1>{`Welcome ${user.firstName}, to your trip planning and tracking page`}</h1>
