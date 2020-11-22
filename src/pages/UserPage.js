eb9e68197585c16377d9ea06527a9ffe2533301c
nmegrant <nme.grant@gmail.com>
2020-11-22 17:21:14 +0100

    Fetch and select user visited tovisit

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,18 +1,31 @@
 import React, { useEffect } from "react";
 import { useHistory } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 import Map from "../components/Map";
 import { selectUser } from "../store/user/selectors";
-import { useSelector } from "react-redux";
+import { fetchUserToVisitThunkCreator } from "../store/userToVisit/actions";
+import { fetchUserVisitedThunkCreator } from "../store/userVisited/actions";
+import { selectUserToVisit } from "../store/userToVisit/selectors";
+import { selectUserVisited } from "../store/userVisited/selectors";
 
 export default function UserPage() {
-  const user = useSelector(selectUser());
   const history = useHistory();
+  const dispatch = useDispatch();
+  const user = useSelector(selectUser());
+  const userToVisit = useSelector(selectUserToVisit());
+  const userVisited = useSelector(selectUserVisited());
 
   useEffect(() => {
     if (user.token === null) {
       history.push("/login");
     }
+    dispatch(fetchUserToVisitThunkCreator);
+    dispatch(fetchUserVisitedThunkCreator);
   }, [user]);
+
+  console.log({ userToVisit });
+  console.log({ userVisited });
+
   return (
     <div>
       <h1>{`Welcome ${user.firstName}, to your trip planning and tracking page`}</h1>
