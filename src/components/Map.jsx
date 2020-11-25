e1382e399ce4894d69d7e8053a495d292c33e1da
nmegrant <nme.grant@gmail.com>
2020-11-25 21:19:23 +0100

    Pass tovisit visited as props

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,16 +1,11 @@
 import React, { useState } from "react";
-import { useSelector } from "react-redux";
 import GoogleMapReact from "google-map-react";
 import Marker from "./Marker/Marker";
-import { selectVisted } from "../store/visited/selectors";
-import { selectToVisit } from "../store/toVisit/selectors";
 
 export default function Map(props) {
+  const { toVisit, visited } = props;
   const [center] = useState({ lat: 41, lng: -36 });
   const [zoom] = useState(0);
-
-  const visited = useSelector(selectVisted());
-  const toVisit = useSelector(selectToVisit());
 
   return (
     <div style={{ height: "200vh", width: "100%", margin: "10px" }}>
@@ -19,24 +14,26 @@ export default function Map(props) {
         defaultCenter={center}
         defaultZoom={zoom}
       >
-        {toVisit.map((place) => (
-          <Marker
-            key={place.id}
-            lat={place.lat}
-            lng={place.long}
-            name={place.city}
-            color="red"
-          />
-        ))}
-        {visited.map((place) => (
-          <Marker
-            key={place.id}
-            lat={place.lat}
-            lng={place.long}
-            name={place.city}
-            color="blue"
-          />
-        ))}
+        {toVisit &&
+          toVisit.map((place) => (
+            <Marker
+              key={place.id}
+              lat={place.lat}
+              lng={place.long}
+              name={place.city}
+              color="red"
+            />
+          ))}
+        {visited &&
+          visited.map((place) => (
+            <Marker
+              key={place.id}
+              lat={place.lat}
+              lng={place.long}
+              name={place.city}
+              color="blue"
+            />
+          ))}
       </GoogleMapReact>
     </div>
   );
