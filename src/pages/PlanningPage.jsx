cdeeb6be5e40b2e2babae4e84559a2f9b9cd090b
nmegrant <nme.grant@gmail.com>
2020-11-27 17:03:19 +0100

    Planning page only for signed in users

--- src/pages/PlanningPage.jsx
+++ src/pages/PlanningPage.jsx
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectUser } from "../store/user/selectors";
 
 export default function PlanningPage() {
+  const history = useHistory();
+  const user = useSelector(selectUser());
+
+  useEffect(() => {
+    if (user.token === null) {
+      history.push("/login");
+    }
+  });
+
   return (
     <div>
       <h1>Plan your next vacation here</h1>
