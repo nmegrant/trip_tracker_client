1960c401b01ddbcfd6f5ec6eaf2cee1ce1179864
nmegrant <nme.grant@gmail.com>
2020-12-18 17:52:16 +0100

    Update trip info to have lat and long

--- src/store/userToVisit/actions.js
+++ src/store/userToVisit/actions.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Geocode from "react-geocode";
 
 export function userToVisitFetched(userToVisitCities) {
   return {
@@ -25,6 +26,18 @@ export function createNewUserToVisitThunkCreator(trip) {
   return async function newUserToVisitThunk(dispatch, getState) {
     const token = localStorage.getItem("token");
     try {
+      Geocode.setApiKey(process.env.REACT_APP_API_KEY);
+      Geocode.setLanguage("en");
+      Geocode.enableDebug();
+      await Geocode.fromAddress(`${trip.city}, ${trip.country}`).then(
+        (response) => {
+          const { lat, lng } = response.results[0].geometry.location;
+          trip = { ...trip, long: lng, lat };
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
       const newTrip = await axios.post(
         `http://localhost:4000/usertovisit`,
         trip,
