986e9407f998cf6552ba2f4ea7c6b124273676cc
nmegrant <nme.grant@gmail.com>
2020-12-18 16:46:08 +0100

    Set up basic form

--- src/pages/PlanningPage.jsx
+++ src/pages/PlanningPage.jsx
@@ -2,6 +2,13 @@ import React, { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { selectUser } from "../store/user/selectors";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import styled from "styled-components";
+
+const FormHolder = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 export default function PlanningPage() {
   const history = useHistory();
@@ -13,9 +20,35 @@ export default function PlanningPage() {
     }
   });
 
+  const handleOnSumbit = (values, { setSubmitting }) => {
+    console.log(values);
+  };
+
   return (
     <div>
       <h1>Plan your next vacation here</h1>
+      <FormHolder>
+        <Formik
+          initialValues={{ city: "", country: "", date: "", days: 0 }}
+          onSubmit={handleOnSumbit}
+        >
+          <Form>
+            <label htmlFor="city">City</label>
+            <Field name="city" type="text" />
+            <ErrorMessage name="city" />
+            <label htmlFor="country">Country</label>
+            <Field name="country" type="text" />
+            <ErrorMessage name="country" />
+            <label htmlFor="date">Arrival Date</label>
+            <Field name="date" type="date" />
+            <ErrorMessage name="date" />
+            <label htmlFor="days">Days</label>
+            <Field name="days" type="number" />
+            <ErrorMessage name="days" />
+            <button type="submit">Submit</button>
+          </Form>
+        </Formik>
+      </FormHolder>
     </div>
   );
 }
