79160759d1df611da876b06934f25d9d437a695c
nmegrant <nme.grant@gmail.com>
2020-12-19 10:27:06 +0100

    Add tabs for tracking and planning

--- src/pages/PlanningPage.jsx
+++ src/pages/PlanningPage.jsx
@@ -4,6 +4,8 @@ import { useSelector, useDispatch } from "react-redux";
 import { selectUser } from "../store/user/selectors";
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import styled from "styled-components";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 import { createNewUserToVisitThunkCreator } from "../store/userToVisit/actions";
 
 const FormHolder = styled.div`
@@ -30,30 +32,40 @@ export default function PlanningPage() {
 
   return (
     <div>
-      <h1>Plan your next vacation here</h1>
-
-      <Formik
-        initialValues={{ city: "", country: "", date: "", days: 0 }}
-        onSubmit={handleOnSumbit}
-      >
-        <Form>
-          <FormHolder>
-            <label htmlFor="city">City</label>
-            <Field name="city" type="text" />
-            <ErrorMessage name="city" />
-            <label htmlFor="country">Country</label>
-            <Field name="country" type="text" />
-            <ErrorMessage name="country" />
-            <label htmlFor="date">Arrival Date</label>
-            <Field name="date" type="date" />
-            <ErrorMessage name="date" />
-            <label htmlFor="days">Days</label>
-            <Field name="days" type="number" />
-            <ErrorMessage name="days" />
-            <button type="submit">Submit</button>
-          </FormHolder>
-        </Form>
-      </Formik>
+      <Tabs>
+        <TabList>
+          <Tab>Plan Upcoming Trips</Tab>
+          <Tab>Record Past Trips</Tab>
+        </TabList>
+        <TabPanel>
+          <h1>Plan your next trip here</h1>
+          <Formik
+            initialValues={{ city: "", country: "", date: "", days: 0 }}
+            onSubmit={handleOnSumbit}
+          >
+            <Form>
+              <FormHolder>
+                <label htmlFor="city">City</label>
+                <Field name="city" type="text" />
+                <ErrorMessage name="city" />
+                <label htmlFor="country">Country</label>
+                <Field name="country" type="text" />
+                <ErrorMessage name="country" />
+                <label htmlFor="date">Arrival Date</label>
+                <Field name="date" type="date" />
+                <ErrorMessage name="date" />
+                <label htmlFor="days">Days</label>
+                <Field name="days" type="number" />
+                <ErrorMessage name="days" />
+                <button type="submit">Submit</button>
+              </FormHolder>
+            </Form>
+          </Formik>
+        </TabPanel>
+        <TabPanel>
+          <h2>Any content 2</h2>
+        </TabPanel>
+      </Tabs>
     </div>
   );
 }
