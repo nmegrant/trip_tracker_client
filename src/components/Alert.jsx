816a06ce539befb17914d8e02a9e1d996f6b8eb6
nmegrant <nme.grant@gmail.com>
2021-01-02 18:40:27 +0100

    Style alert box

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,14 +1,25 @@
 import React from "react";
+import styled from "styled-components";
 import { useSelector } from "react-redux";
 import { selectMessage } from "../store/appState//selectors";
 
 export default function Alert() {
   const message = useSelector(selectMessage());
 
+  const AlertBox = message
+    ? styled.div`
+        border: solid 1px ${message.style};
+        background-color: rgba(0, 255, 0, 0.1);
+        color: ${message.style};
+      `
+    : null;
+
   return (
     <div>
-      {message !== null ? (
-        <h1 style={{ color: message.style }}>{message.message}</h1>
+      {message ? (
+        <AlertBox>
+          <p>{message.message}</p>
+        </AlertBox>
       ) : null}
     </div>
   );
