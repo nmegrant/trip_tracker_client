fd8e5b731dab7584030565ba16bd4f33b588d907
nmegrant <nme.grant@gmail.com>
2021-01-02 17:23:27 +0100

    Add messageInfo to appState reducer

--- src/store/appState/reducer.js
+++ src/store/appState/reducer.js
@@ -1,11 +1,25 @@
 const initialState = {
   darkMode: false,
+  messageInfo: {
+    style: null,
+    message: null,
+  },
 };
 
 export default function appStateReducer(state = initialState, action) {
   switch (action.type) {
     case "DARK_MODE_SET":
       return { ...state, darkMode: action.payload };
+    case "SET_MESSAGE":
+      return { ...state, messageInfo: action.payload };
+    case "CLEAR_MESSAGE":
+      return {
+        ...state,
+        messageInfo: {
+          style: null,
+          message: null,
+        },
+      };
     default:
       return state;
   }
